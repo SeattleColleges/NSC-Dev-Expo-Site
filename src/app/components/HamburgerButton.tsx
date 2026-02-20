5b4a36d637abc6a31dba2cab98b773dec0e0f7a1
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-02-19 19:57:25 -0800

    Add dropdown outline and arrow icons

--- src/app/components/HamburgerButton.tsx
+++ src/app/components/HamburgerButton.tsx
@@ -1,6 +1,6 @@
-import { View, Pressable, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { GestureResponderEvent } from  "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 type Props = {
   onPress: (event: GestureResponderEvent) => void;
@@ -9,14 +9,14 @@ type Props = {
 export default function HamburgerButton({ onPress }: Props) {
   return (
     <Pressable onPress={onPress} style={styles.button}>
-      <MaterialIcons name="menu" size={24} color="black" />
+      <Feather name="menu" size={62} color="black" />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    padding: 8,
+    paddingInline: 10,
     justifyContent: 'center',
   }
 });
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { View, Text, Pressable, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import { useState } from "react";
 import HamburgerButton from "./HamburgerButton";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -18,11 +19,25 @@ export default function Navbar() {
 
             {menuOpen && (
                 <View style={styles.dropdown}>
-                    <Text>Home</Text>
-                    <Text>Contact</Text>
-                    <Text>People</Text>
-                    <Text>Projects</Text>
-                    <Text>Donate</Text>
+                    <View style={styles.dropdownCategory}>
+                        <Text style={styles.dropdownText}>About</Text>
+                        <MaterialIcons name="keyboard-arrow-up" size={56} color="black" />
+                    </View>
+                    <View style={styles.dropdownCategory}>
+                        <Text style={styles.dropdownText}>Contact</Text>
+                        <MaterialIcons name="keyboard-arrow-up" size={56} color="black" />
+                    </View>
+                    <View style={styles.dropdownCategory}>
+                        <Text style={styles.dropdownText}>People</Text>
+                        <MaterialIcons name="keyboard-arrow-up" size={56} color="black" />
+                    </View>
+                    <View style={styles.dropdownCategory}>
+                        <Text style={styles.dropdownText}>Projects</Text>
+                        <MaterialIcons name="keyboard-arrow-up" size={56} color="black" />
+                    </View>                    <View style={styles.dropdownCategory}>
+                        <Text style={styles.dropdownText}>Donate</Text>
+                        <MaterialIcons name="keyboard-arrow-up" size={56} color="black" />
+                    </View>
                 </View>
             )}
         </>
@@ -31,24 +46,40 @@ export default function Navbar() {
 
 const styles = StyleSheet.create({
     container: {
-        height: 60,
-        paddingHorizontal: 20,
+        height: 80,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         backgroundColor: '#fff',
+        margin: 8,
+        paddingLeft: 12
     },
     logoBox: {
         backgroundColor: "#525252",
+        width: 65,
+        height: 65,
+        justifyContent: "center"
     },
     logo: {
         color:"#fff",
-        fontSize: 18,
+        fontSize: 20,
+        alignSelf: "center"
     },
     dropdown: {
-        backgroundColor: '#fff',
+        backgroundColor: "#383838",
         padding: 20,
         borderBottomWidth: 1,
         borderColor: '#eee',
     },
+    dropdownText: {
+        fontSize: 48,
+        color: "#DDD"
+    },
+    dropdownCategory: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        gap: 4
+    }
 });
