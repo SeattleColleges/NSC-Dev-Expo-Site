60030e886210e65a7018b98645162f20521be210
BHabibMeetKai <bade.habib@meetkai.com>
2026-02-21 18:00:06 -0800

    Implemented client section

--- src/app/(pages)/index.tsx
+++ src/app/(pages)/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import ClientSection from 'src/components/clientSection';
 
 export default function HomePage() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to the Home Page!</Text>
+      <ClientSection />
     </View>
   );
 }
