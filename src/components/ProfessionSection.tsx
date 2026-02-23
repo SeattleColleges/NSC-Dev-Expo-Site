4c0e52ab2ca0fe186e06badd2cc4e42a7bfd677d
Dara Sen <darasen0277@gmail.com>
2026-02-23 00:43:46 -0800

    Add profession section components

---
+++ src/components/ProfessionSection.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+
+
+<div>
+    <h1>Possible Professions</h1>
+
+
+    <h2>Software Developer</h2>
+
+    <p>Applies engineering principles to design, develop, test, and maintain software systems./</p>
+    
+    <p>Going beyond just coding to manage the entire software development life cycle for apps, operating systems, or complex cloud solutions, ensuring they are robust, scalable, and meet user needs by collaborating with teams.</p>
+    
+
+    <h2>Mobile App Developer</h2>
+
+    <p>A software professional who designs, builds, and maintains applications for mobile devices like smartphones and tablets.</p>
+
+
+    <h2>Back-End Developer</h2>
+
+    <p>Build and maintains the server-side logic of a web application, focusing on the technology that users don't see, such as databases, server architecture, and application programming interfaces (APIs).</p>
+
+    <p>They are responsible for the underlying structure, ensuring data is stored, retrieved, and processed correctly, securely, and efficiently.</p>
+</div>
+
