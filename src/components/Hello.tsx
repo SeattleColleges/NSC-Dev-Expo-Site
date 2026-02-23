0c54ce041e48391b658e8f51dde4f801d08ec5c0
Dara Sen <darasen0277@gmail.com>
2026-02-23 08:54:59 -0800

    add sections

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,13 @@
 
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import ProfessionSection from "./ProfessionSection";
 
 export default function Hello() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello from the helloFunc component file!</Text>
-      <ProfessionSection />
-    </View>
+    <ScrollView>
+      <View>
+        <ProfessionSection />
+      </View>
+    </ScrollView>
   );
 }
--- src/components/ProfessionSection.tsx
+++ src/components/ProfessionSection.tsx
@@ -11,6 +11,15 @@ function ProfessionSection() {
                 <Text>Applies engineering principles to design, develop, test, and maintain software systems.</Text>
                 <Text>Going beyond just coding to manage the entire software development life cycle for apps, operating systems, or complex cloud solutions, ensuring they are robust, scalable, and meet user needs by collaborating with teams.</Text>
             </View>
+            <View>
+                <Text>Mobile App Developer</Text>
+                <Text>A software professional who designs, builds, and maintains applications for mobile devices like smartphones and tablets.</Text>
+            </View>
+            <View>
+                <Text>Back-End Engineer</Text>
+                <Text>Builds and maintains the server-side logic of a web application, focusing on the technology that users don't see, such as databases, server architecture, and application programming interfaces (APIs).</Text>
+                <Text>The are responsible for teh underlying structure, ensuring data is stored, retrieved, and processed correctly, securely, and efficiently.</Text>
+            </View>
         </ScrollView>
     );
 }
