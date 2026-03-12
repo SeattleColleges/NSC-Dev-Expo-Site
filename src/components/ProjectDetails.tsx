142cd20ab318b7d26fc5f6a3347ff91eaea56a40
Evelyn Bushell <155328177+Eveybushell@users.noreply.github.com>
2026-03-12 15:45:35 -0700

    Update ProjectDetails.tsx

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -3,43 +3,45 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 export default function ProjectDetails() {
     return (
         <View>
-            <View style={styles.topContainer}>
-                <Text style={styles.h1}>
-                    The Goal
-                </Text>
-                <View></View>
-                <Text style={styles.body}>
-                    Is to offer essential support to students within the broader Seattle Colleges system,
-                    ensuring they have access to professional clothing for interviews, employment, and graduation ceremonies.
-                    Emphasizing its not-for-profit nature, these clothing items come without a financial burden to the student.
-                </Text>
-            </View>
-            <View style={styles.bottomContainer}>
-                <View style={styles.imageBox}>
-                    <Image>
-
-                    </Image>
-                    <Text style={styles.imageText}>
-                    Marketing image*
+            <View style={styles.mainContainer}>
+                <View style={styles.topContainer}>
+                    <Text style={styles.h1}>
+                        The Goal
+                    </Text>
+                    <View style={styles.underLine}></View>
+                    <Text style={styles.body}>
+                        Is to offer essential support to students within the broader Seattle Colleges system,
+                        ensuring they have access to professional clothing for interviews, employment, and graduation ceremonies.
+                        Emphasizing its not-for-profit nature, these clothing items come without a financial burden to the student.
                     </Text>
                 </View>
-                <Text style={styles.imageCaption}>
-                    Captioning here*
-                </Text>
+                <View style={styles.bottomContainer}>
+                    <View style={styles.imageBox}>
+                        <Image>
+
+                        </Image>
+                        <Text style={styles.imageText}>
+                        Marketing image*
+                        </Text>
+                    </View>
+                    <Text style={styles.imageCaption}>
+                        Captioning here*
+                    </Text>
+                </View>
             </View>
         </View>
-    )
+    )   
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
         backgroundColor: '#484848',
         flex: 1,
-        justifyContent: 'center',
+        alignItems: 'center',
         width: 700,
     },
     topContainer: {
-        marginVertical: '10%',
+        margin: '5%',
         width: 600,
     },
     h1: {
@@ -52,20 +54,26 @@ const styles = StyleSheet.create({
         height: 2,
         width: 220,
         backgroundColor: '#E0E0E0',
+        marginVertical: 10,
     },
     body: {
         fontFamily: 'Roboto',
         color: '#E5E5E5',
         fontSize: 20,
+        marginVertical: 5,
     },
     bottomContainer: {
-
+        width:481,
+        height: 415,
+        marginBottom: 64,
     },
     imageBox: {
         backgroundColor: '#CCCCCC',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        width: 481,
+        height: 379,
     },
     imageText: {
         fontFamily: 'Roboto',
@@ -76,5 +84,6 @@ const styles = StyleSheet.create({
         color: '#DDDDDD',
         fontSize: 24,
         fontStyle: 'italic',
+        marginVertical: 5,
     },
 })
