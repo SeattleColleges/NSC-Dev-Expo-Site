567140f797132b67d5718f86c00b3da628bf1403
amdemw206-coder <amdemw206@gmail.com>
2026-03-21 11:03:44 -0700

    Hero and Scrollbar Support

--- src/app/(pages)/currentstudent.tsx
+++ src/app/(pages)/currentstudent.tsx
@@ -1,12 +1,18 @@
 import { Card } from "src/components/CardStudentPageCurrent";
 import Title from "src/components/CurrentStudentTitle"
-
+import Hero from "src/components/HeroCurrentStudent"
+import { ScrollView } from "react-native"
 export default function currentStudentPage(){
     return(
         <>
-        <Title/>
-        <Card/>
-        <Card/>
+        <ScrollView>
+            <Hero/>
+            <Title/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </ScrollView>
         </>
         
     )
---
+++ src/components/HeroCurrentStudent.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+export default function Hero(){
+    return(
+    <LinearGradient 
+        colors={['#1A1A1A', '#d9d9d9b9']}
+        start={{ x: 0, y: 0.5 }} 
+        end={{ x: 1, y: 0.5 }}
+        style={styles.bgHero}> 
+        <Text style={styles.textHero}>See the talent of the future</Text>
+    </LinearGradient>
+    )
+}
+
+const styles = StyleSheet.create({
+    bgHero:{
+        height: 117,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        paddingBottom: 12
+    }, 
+    
+    textHero:{
+        fontFamily:'roboto',
+        fontWeight: 700,
+        fontSize: 40, 
+        letterSpacing:0,
+        color: '#E8E8E8'
+    }
+})
