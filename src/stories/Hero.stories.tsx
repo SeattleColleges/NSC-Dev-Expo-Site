13c05080c6e0ce17eef0c595ad8ab28f8f375ee9
Ilikemyhome <arenasvivi7@gmail.com>
2026-04-22 18:36:47 -0700

    created hero.stories.tsx

---
+++ src/stories/Hero.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Hero from "../components/Hero";
+
+const meta: Meta<typeof Hero> = {
+  title: "Components/Hero",
+  component: Hero,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Hero>;
+
+export const Default: Story = {
+  args: {},
+};
