5882d1c6c8f0ae0441af1294aad35c4f26f89f0e
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2026-04-28 15:43:22 -0700

    Add files via upload
    change "console.log": added new function handleJoinPress()

---
+++ Hero.tsx
@@ -0,0 +1,66 @@
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import HeroCTAButton from "./HeroCTAButton";
+
+import { Alert } from "react-native";
+
+function handleJoinPress() {
+    Alert.alert("Join Us", "Button pressed!");
+}
+
+export default function Hero() {
+    return(
+        <View style={styles.hero}>
+            <LinearGradient
+                colors={["#1A1A1A", "#808080"]}
+                style={styles.gradientBackground}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+            >
+                <View style={styles.textContainer}>
+                    <Text style={styles.headingText}>North Seattle Application Development</Text>
+                    <Text style={styles.subHeadingText}>Developing IT students of the future</Text>
+                </View>
+            </LinearGradient>
+            <HeroCTAButton 
+                accessibilityRole="button"
+                label="Join Us"
+                //onPress={showAlert}
+                onPress={handleJoinPress}
+                style={styles.floatingButton} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    hero: {
+        backgroundColor: "white",
+        paddingTop: 60,
+        // paddingVertical: 24,
+
+    },
+    textContainer: {
+        paddingLeft: 24,
+        paddingVertical: 24,
+    },
+    headingText: {
+        fontSize: 36,
+        color: "#E5E5E5",
+        // fontFamily: "Roboto"
+        fontWeight: "600",
+        paddingBottom: 36
+    },
+    subHeadingText: {
+        fontSize: 16,
+        color: "#E5E5E5",
+        // fontFamily: "Inter"
+    },
+    gradientBackground: {
+        flex: 1,
+        paddingVertical: 24,
+    },
+    
+    floatingButton: {
+        margin: 24
+    },
+});
