c1344ab4700e577a4a6e8b4f4596ebb42ee3f691
Fathiya Yoosef <155487698+Dhool143@users.noreply.github.com>
2026-05-13 10:19:18 -0700

    Add TechStackBanner Comopnent

---
+++ .rnstorybook/index.tsx
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+import { View, Text} from  "react-native";
+
+export default function StorybookUIRoot(){
+
+return (
+
+    <View>
+
+  <Text>Storybook</Text>
+
+
+    </View>
+
+
+);
+
+}
