b385d875512c36fc6aadc9beb42dca90d8f414da
Fathiya Yoosef <155487698+Dhool143@users.noreply.github.com>
2026-05-27 19:42:51 -0700

    Fix TechStackBanner Storybook story

---
+++ src/components/TechStackBanner.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TechStackBanner from "./TechStackBanner";
+
+export default {
+  title: "Components/TechStackBanner",
+  component: TechStackBanner,
+};
+
+export const Default = () => <TechStackBanner />;
